125359d9ac2b5e06899a326f2bf10593c7be2e61
krooks_geek <60033390+krooksgeek@users.noreply.github.com>
2020-10-02 12:45:25 +0530

    Update binomialheap.c

--- binomialheap.c
+++ binomialheap.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<malloc.h>
+ 
 struct node {
     int n;
     int degree;
@@ -7,65 +8,44 @@ struct node {
     struct node* child;
     struct node* sibling;
 };
-
+ 
+struct node* MAKE_bin_HEAP();
+int bin_LINK(struct node*, struct node*);
+struct node* CREATE_NODE(int);
+struct node* bin_HEAP_UNION(struct node*, struct node*);
+struct node* bin_HEAP_INSERT(struct node*, struct node*);
+struct node* bin_HEAP_MERGE(struct node*, struct node*);
+struct node* bin_HEAP_EXTRACT_MIN(struct node*);
+int REVERT_LIST(struct node*);
+int DISPLAY(struct node*);
+struct node* FIND_NODE(struct node*, int);
+int bin_HEAP_DECREASE_KEY(struct node*, int, int);
+int bin_HEAP_DELETE(struct node*, int);
 int count = 1;
-
+ 
 struct node* MAKE_bin_HEAP() {
     struct node* np;
     np = NULL;
     return np;
 }
-
+ 
 struct node * H = NULL;
 struct node *Hr = NULL;
-
+ 
 int bin_LINK(struct node* y, struct node* z) {
     y->parent = z;
     y->sibling = z->child;
     z->child = y;
     z->degree = z->degree + 1;
 }
-
+ 
 struct node* CREATE_NODE(int k) {
-    struct node* p;
+    struct node* p;//new node;
     p = (struct node*) malloc(sizeof(struct node));
     p->n = k;
     return p;
 }
-
-struct node* bin_HEAP_MERGE(struct node* H1, struct node* H2) {
-    struct node* H = MAKE_bin_HEAP();
-    struct node* y;
-    struct node* z;
-    struct node* a;
-    struct node* b;
-    y = H1;
-    z = H2;
-    if (y != NULL) {
-        if (z != NULL && y->degree <= z->degree)
-            H = y;
-        else if (z != NULL && y->degree > z->degree)
-            H = z;
-        else
-            H = y;
-    } else
-        H = z;
-    while (y != NULL && z != NULL) {
-        if (y->degree < z->degree) {
-            y = y->sibling;
-        } else if (y->degree == z->degree) {
-            a = y->sibling;
-            y->sibling = z;
-            y = a;
-        } else {
-            b = z->sibling;
-            z->sibling = y;
-            z = b;
-        }
-    }
-    return H;
-}
-
+ 
 struct node* bin_HEAP_UNION(struct node* H1, struct node* H2) {
     struct node* prev_x;
     struct node* next_x;
@@ -99,7 +79,7 @@ struct node* bin_HEAP_UNION(struct node* H1, struct node* H2) {
     }
     return H;
 }
-
+ 
 struct node* bin_HEAP_INSERT(struct node* H, struct node* x) {
     struct node* H1 = MAKE_bin_HEAP();
     x->parent = NULL;
@@ -110,8 +90,58 @@ struct node* bin_HEAP_INSERT(struct node* H, struct node* x) {
     H = bin_HEAP_UNION(H, H1);
     return H;
 }
-
-
+ 
+struct node* bin_HEAP_MERGE(struct node* H1, struct node* H2) {
+    struct node* H = MAKE_bin_HEAP();
+    struct node* y;
+    struct node* z;
+    struct node* a;
+    struct node* b;
+    y = H1;
+    z = H2;
+    if (y != NULL) {
+        if (z != NULL && y->degree <= z->degree)
+            H = y;
+        else if (z != NULL && y->degree > z->degree)
+            /* need some modifications here;the first and the else conditions can be merged together!!!! */
+            H = z;
+        else
+            H = y;
+    } else
+        H = z;
+    while (y != NULL && z != NULL) {
+        if (y->degree < z->degree) {
+            y = y->sibling;
+        } else if (y->degree == z->degree) {
+            a = y->sibling;
+            y->sibling = z;
+            y = a;
+        } else {
+            b = z->sibling;
+            z->sibling = y;
+            z = b;
+        }
+    }
+    return H;
+}
+ 
+int DISPLAY(struct node* H) {
+    struct node* p;
+    if (H == NULL) {
+        printf("\nHEAP EMPTY");
+        return 0;
+    }
+    printf("\nTHE ROOT NODES ARE:-\n");
+    p = H;
+    while (p != NULL) {
+        printf("%d", p->n);
+        if (p->sibling != NULL)
+            printf("-->");
+        p = p->sibling;
+    }
+    printf("\n");
+}
+ 
 struct node* bin_HEAP_EXTRACT_MIN(struct node* H1) {
     int min;
     struct node* t = NULL;
@@ -123,6 +153,7 @@ struct node* bin_HEAP_EXTRACT_MIN(struct node* H1) {
         printf("\nNOTHING TO EXTRACT");
         return x;
     }
+    //    int min=x->n;
     p = x;
     while (p->sibling != NULL) {
         if ((p->sibling)->n < min) {
@@ -147,7 +178,7 @@ struct node* bin_HEAP_EXTRACT_MIN(struct node* H1) {
     H = bin_HEAP_UNION(H1, Hr);
     return x;
 }
-
+ 
 int REVERT_LIST(struct node* y) {
     if (y->sibling != NULL) {
         REVERT_LIST(y->sibling);
@@ -156,7 +187,7 @@ int REVERT_LIST(struct node* y) {
         Hr = y;
     }
 }
-
+ 
 struct node* FIND_NODE(struct node* H, int k) {
     struct node* x = H;
     struct node* p = NULL;
@@ -167,11 +198,13 @@ struct node* FIND_NODE(struct node* H, int k) {
     if (x->child != NULL && p == NULL) {
         p = FIND_NODE(x->child, k);
     }
+ 
     if (x->sibling != NULL && p == NULL) {
         p = FIND_NODE(x->sibling, k);
     }
     return p;
 }
+ 
 int bin_HEAP_DECREASE_KEY(struct node* H, int i, int k) {
     int temp;
     struct node* p;
@@ -198,134 +231,96 @@ int bin_HEAP_DECREASE_KEY(struct node* H, int i, int k) {
     }
     printf("\nKEY REDUCED SUCCESSFULLY!");
 }
-
+ 
 int bin_HEAP_DELETE(struct node* H, int k) {
     struct node* np;
     if (H == NULL) {
         printf("\nHEAP EMPTY");
         return 0;
     }
+ 
     bin_HEAP_DECREASE_KEY(H, k, -1000);
     np = bin_HEAP_EXTRACT_MIN(H);
     if (np != NULL)
         printf("\nNODE DELETED SUCCESSFULLY");
 }
-
-void DISPLAY(struct node* H) {
-    struct node* p;
-    if (H == NULL) {
-        printf("\nHEAP EMPTY");
-    }
-    printf("\nTHE ROOT NODES ARE:-\n");
-    p = H;
-    while (p != NULL) {
-        printf("%d", p->n);
-        if (p->sibling != NULL)
-            printf("-->");
-        p = p->sibling;
-    }
-    printf("\n");
-}
-
-
+ 
 int main() {
     int i, n, m, l;
     struct node* p;
     struct node* np;
-    char ch ;
-    int choice;
-    printf("\nEnter the no. of elements:");
+    char ch;
+    printf("\nENTER THE NUMBER OF ELEMENTS:");
     scanf("%d", &n);
-    printf("\nEnter elements:\n");
+    printf("\nENTER THE ELEMENTS:\n");
     for (i = 1; i <= n; i++) {
         scanf("%d", &m);
         np = CREATE_NODE(m);
         H = bin_HEAP_INSERT(H, np);
     }
     DISPLAY(H);
-do{
-    printf("Main Menu:\n");
-    printf("\n1.Min Heap\n2.Max Heap\n3.Quit\n");
-    scanf("%d",&choice);
-    switch(choice){
-    case 1:
     do {
         printf("\nMENU:-\n");
-        printf("\n1)Insert\n2)Delete \n3)quit\n");
+        
+        printf(
+                "\n1)INSERT AN ELEMENT\n2)EXTRACT THE MINIMUM KEY NODE\n3)DECREASE A NODE KEY\n 4)DELETE A NODE\n5)QUIT\n");
         scanf("%d", &l);
         switch (l) {
         case 1:
             do {
-                printf("\nEnter the element to be inserted:");
+                printf("\nENTER THE ELEMENT TO BE INSERTED:");
                 scanf("%d", &m);
                 p = CREATE_NODE(m);
                 H = bin_HEAP_INSERT(H, p);
-                printf("\nNow heap is:\n");
+                printf("\nNOW THE HEAP IS:\n");
                 DISPLAY(H);
-                printf("\nContinue Insertion? Press(m/M)=\n");
+                printf("\nINSERT MORE(y/Y)= \n");
                 fflush(stdin);
                 scanf("%c", &ch);
-            } while (ch == 'M' || ch == 'm');
+            } while (ch == 'Y' || ch == 'y');
             break;
         case 2:
             do {
-                printf("\nEnter key to be deleted: ");
-                scanf("%d", &m);
-                bin_HEAP_DELETE(H, m);
-                printf("\nContinue Deletion? press(m/M)\n");
-                fflush(stdin);
-                scanf("%c", &ch);
-            } while (ch == 'm' || ch == 'M');
-            break;
-        case 3:
-            break;
-        default:
-            printf("\nOPPS! Invalid Entry.\n");
-        }
-    } while (l != 3);
-    break;
-
-    case 2:
-        do {
-        printf("\nMENU:-\n");
-        printf("\n1)Insert\n2)Delete \n3)quit\n");
-        scanf("%d", &l);
-        switch (l) {
-        case 1:
-            do {
-                printf("\nEnter the element to be inserted:");
-                scanf("%d", &m);
-                p = CREATE_NODE(m);
-                H = bin_HEAP_INSERT(H, p);
-                printf("\nNow heap is:\n");
+                printf("\nEXTRACTING THE MINIMUM KEY NODE");
+                p = bin_HEAP_EXTRACT_MIN(H);
+                if (p != NULL)
+                    printf("\nTHE EXTRACTED NODE IS %d", p->n);
+                printf("\nNOW THE HEAP IS:\n");
                 DISPLAY(H);
-                printf("\nContinue Insertion? Press(m/M)=\n");
+                printf("\nEXTRACT MORE(y/Y)\n");
                 fflush(stdin);
                 scanf("%c", &ch);
-            } while (ch == 'M' || ch == 'm');
-            break;
-        case 2:
-            do {
-                printf("\nEnter key to be deleted: ");
-                scanf("%d", &m);
-                bin_HEAP_DELETE(H, m);
-                printf("\nContinue Deletion? press(m/M)\n");
-                fflush(stdin);
-                scanf("%c", &ch);
-            } while (ch == 'm' || ch == 'M');
+            } while (ch == 'Y' || ch == 'y');
             break;
         case 3:
+            do {
+                printf("\nENTER THE KEY OF THE NODE TO BE DECREASED:");
+                scanf("%d", &m);
+                printf("\nENTER THE NEW KEY : ");
+                scanf("%d", &l);
+                bin_HEAP_DECREASE_KEY(H, m, l);
+                printf("\nNOW THE HEAP IS:\n");
+                DISPLAY(H);
+                printf("\nDECREASE MORE(y/Y)\n");
+                fflush(stdin);
+                scanf("%c", &ch);
+            } while (ch == 'Y' || ch == 'y');
+            break;
+        case 4:
+            do {
+                printf("\nENTER THE KEY TO BE DELETED: ");
+                scanf("%d", &m);
+                bin_HEAP_DELETE(H, m);
+                printf("\nDELETE MORE(y/Y)\n");
+                fflush(stdin);
+                scanf("%c", &ch);
+            } while (ch == 'y' || ch == 'Y');
+            break;
+        case 5:
+            printf("\nTHANK U SIR\n");
             break;
         default:
-            printf("\nOPPS! Invalid Entry.\n");
+            printf("\nINVALID ENTRY...TRY AGAIN....\n");
         }
-    } while (l != 3);
-    break;
-
-    case 3:
-            break;
-        default:
-            printf("\nOPPS! Invalid Entry.\n");
-    }
-}while (choice != 3);
+    } while (l != 5);
 }
